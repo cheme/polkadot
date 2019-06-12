65d2a817be5d767251d060dbdc043bf9058d7d26
cheme <emericchevalier.pro@gmail.com>
2019-06-12 12:54:52 +0200

    not working properly

--- node-template/runtime/src/template.rs
+++ node-template/runtime/src/template.rs
@@ -8,11 +8,27 @@
 /// For more guidance on Substrate modules, see the example module
 /// https://github.com/paritytech/substrate/blob/master/srml/example/src/lib.rs
 
-use support::{decl_module, decl_storage, decl_event, StorageValue, dispatch::Result};
+// Encoding library
+use parity_codec::Encode;
+
+// Enables access to store a value in runtime storage
+// Imports the `Result` type that is returned from runtime functions
+// Imports the `decl_module!` and `decl_storage!` macros
+use support::{StorageValue, dispatch::Result, decl_module, decl_storage, decl_event};
+
+// Traits used for interacting with Substrate's Balances module
+// `Currency` gives you access to interact with the on-chain currency
+// `WithdrawReason` and `ExistenceRequirement` are enums for balance functions
+use support::traits::{Currency, WithdrawReason, ExistenceRequirement};
+
+// These are traits which define behavior around math and hashing
+use runtime_primitives::traits::{Zero, Hash, Saturating};
+
+// Enables us to verify an call to our module is signed by a user account
 use system::ensure_signed;
 
 /// The module's configuration trait.
-pub trait Trait: system::Trait {
+pub trait Trait: balances::Trait {
 	// TODO: Add other types and constants required configure this module.
 
 	/// The overarching event type.
@@ -26,6 +42,9 @@ decl_storage! {
 		// Here we are declaring a StorageValue, `Something` as a Option<u32>
 		// `get(something)` is the default getter which returns either the stored `u32` or `None` if nothing stored
 		Something get(something): Option<u32>;
+    Payment get(payment): Option<T::Balance>;
+    Pot get(pot): T::Balance;
+    Nonce get(nonce): u64;
 	}
 }
 
@@ -52,6 +71,61 @@ decl_module! {
 			Self::deposit_event(RawEvent::SomethingStored(something, who));
 			Ok(())
 		}
+
+
+    fn set_payment(origin, value: T::Balance) -> Result {
+
+      // Ensure that the function call is a signed message (i.e. a transaction)
+      let _ = ensure_signed(origin)?;
+      // If `payment` is not initialized with some value
+      if Self::payment().is_none() {
+        // Set the value of `payment`
+        <Payment<T>>::put(value);
+        // Initialize the `pot` with the same value
+        <Pot<T>>::put(value);
+      }
+      // Return Ok(()) when everything happens successfully
+      Ok(())
+    }
+    
+    fn play(origin) -> Result {
+      // Logic for playing the game
+      // Ensure that the function call is a signed message (i.e. a transaction)
+	// Additionally, derive the sender address from the signed message
+	let sender = ensure_signed(origin)?;
+	// Ensure that `payment` storage item has been set
+	let payment = Self::payment().ok_or("Must have payment amount set")?;
+	// Read our storage values, and place them in memory variables
+	let mut nonce = Self::nonce();
+	let mut pot = Self::pot();
+
+	// Try to withdraw the payment from the account, making sure that it will not kill the account
+	let _ = <balances::Module<T> as Currency<_>>::withdraw(&sender, payment, WithdrawReason::Reserve, ExistenceRequirement::KeepAlive)?;
+
+	// Generate a random hash between 0-255 using a csRNG algorithm
+	if (<system::Module<T>>::random_seed(), &sender, nonce)
+		.using_encoded(<T as system::Trait>::Hashing::hash)
+		.using_encoded(|e| e[0] < 128)
+	{
+		// If the user won the coin flip, deposit the pot winnings; cannot fail
+		let _ = <balances::Module<T> as Currency<_>>::deposit_into_existing(&sender, pot)
+		.expect("`sender` must exist since a transaction is being made and withdraw will keep alive; qed.");
+		// Reduce the pot to zero
+		pot = Zero::zero();
+	}
+
+	// No matter the outcome, increase the pot by the payment amount
+	pot = pot.saturating_add(payment);
+	// Increment the nonce
+	nonce = nonce.wrapping_add(1);
+
+	// Store the updated values for our module
+	<Pot<T>>::put(pot);
+	<Nonce<T>>::put(nonce);
+
+	// Return Ok(()) when everything happens successfully
+	Ok(())
+    }
 	}
 }
 
