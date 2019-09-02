f4726c1f0b796315e2822542dc86ddc91380cad2
Emeric Chevalier <emericchevalier.pro@gmail.com>
2019-09-02 17:34:23 +0200

    Use two counters (prospective one only correct, committed all less correct).

--- core/utils/historied-data/src/tree.rs
+++ core/utils/historied-data/src/tree.rs
@@ -33,6 +33,21 @@
 // New branches uses a + 1 commmited counter (meaning prospective).
 // Also on commit we branch an new prospective one (counter +1).
 // discard prospective = increase prospective counter.
+//
+// -> 2 counter:
+// - committed : every commited are valid value (for block see it as finalized),
+// so everithing less or equal is valid.
+// Only one branching path should be commited, so prospective value are U32::max_value.
+// And we put an actual committed counter to branch on commit
+//
+// For branch case: some pruning is done over those indexed value (so by commit_ix (similar to
+// a block nb) -> therefore we do not use a boolean value).
+//
+// - prospective : this is only valid for latest prospective value, is before commited. 
+//   - on drop prospective: +1 counter prospective meaning no prospective valid anymore
+//   - on commit: prospective = commited index + 1 and update commited index of commited branch (so the commited
+//   value remain).
+//
 
 use crate::{
 	State as TransactionState,
@@ -47,7 +62,7 @@ use rstd::vec::Vec;
 use rstd::collections::btree_map::BTreeMap;
 
 // TODO could benefit from smallvec!! need an estimation
-// of number of node (it still stores a usize + a smallvec)
+// of number of node (it still stores a usize + a smallvec) 
 #[derive(Debug, Clone)]
 #[cfg_attr(any(test, feature = "test"), derive(PartialEq))]
 pub struct History<V>(Vec<HistoryBranch<V>>);
@@ -73,16 +88,27 @@ pub struct States {
 }
 
 impl StatesBranch {
-  pub fn is_committed(&self, states: &States) -> bool {
-    self.state_ix == states.committed_ix
-  }
-  pub fn is_prospective(&self, states: &States) -> bool {
-    self.state_ix > states.prospective_ix
-  }
-  pub fn is_dropped(&self, states: &States) -> bool {
-    self.state_ix != states.committed_ix
-    && self.state_ix < states.prospective_ix
-  }
+	pub fn is_committed(&self, states: &States) -> bool {
+		self.is_committed_internal(states.committed_ix)
+	}
+	fn is_committed_internal(&self, committed_ix: usize) -> bool {
+		self.committed_ix <= committed_ix
+	}
+	
+	pub fn is_prospective(&self, states: &States) -> bool {
+		self.is_prospective_internal(states.prospective_ix)
+	}
+	fn is_prospective_internal(&self, prospective_ix: usize) -> bool {
+		self.prospective_ix >= prospective_ix
+	}
+
+	fn is_dropped_internal(&self, prospective_ix: usize, committed_ix: usize) -> bool {
+		!self.is_committed_internal(committed_ix)
+			&& !self.is_prospective_internal(prospective_ix)
+	}
+	pub fn is_dropped(&self, states: &States) -> bool {
+		self.is_dropped_internal(states.prospective_ix, states.committed_ix)
+	}
 }
 
 impl Default for States {
@@ -91,10 +117,11 @@ impl Default for States {
 			branches: Default::default(),
 			last_branch_ix: 0,
 			committed_ix: 0,
-			prospective_ix: 1,
+			prospective_ix: 0,
 		}
 	}
 }
+
 #[derive(Debug, Clone)]
 #[cfg_attr(any(test, feature = "test"), derive(PartialEq))]
 pub struct StatesBranch {
@@ -102,17 +129,16 @@ pub struct StatesBranch {
 	// content pointing to it even if it seems safe to reuse a previously
 	// use ix).
 	branch_ix: usize,
-  // status of the branch, if the index is less than
-  // states committed_ix, then the full branch is not in
-  // the committed path, and seen as dropped.
-  // in between committed and prospective ix it is also a dropped prospective index.
-	state_ix: usize,
-
+	
 	origin_branch_ix: usize,
 
-  // when a branch has children it cannot be change (get_mut return none
-  // when get return something).
+	// when a branch has children it cannot be change (get_mut return none
+	// when get return something).
 	has_children: bool,
+
+	prospective_ix: usize,
+
+	committed_ix: usize,
 
 	state: LinearStates,
 }
@@ -127,7 +153,9 @@ pub type StatesRef = Vec<(usize, StatesBranch)>;
 
 impl States {
 
-	pub fn clear(&mut self) {
+	/// clear state but keep existing branch values (can be call after a full gc:
+	/// enforcing no commited containing dropped values).
+	pub fn unsafe_clear(&mut self) {
 		self.branches.clear();
 		self.last_branch_ix = 0;
 	}
@@ -137,31 +165,41 @@ impl States {
 	}
 
 	// create a branches. End current branch.
-  // Return first created index (next branch are sequential indexed)
-  // or None if origin branch does not allow branch creation (commited branch or non existing).
+	// Return first created index (next branch are sequential indexed)
+	// or None if origin branch does not allow branch creation (commited branch or non existing).
 	pub fn create_branch(
 		&mut self,
 		branch_ix: usize,
 		nb_branch: usize,
 	) -> Option<usize> {
-		// empty case
-		if self.last_branch_ix == 0 {
-			self.last_branch_ix = 1;
-      for i in 1..nb_branch + 1 {
-        self.branches.insert(1, StatesBranch {
-          branch_ix: i,
-          origin_branch_ix: 0,
-          state_ix: self.prospective_ix,
-          has_children: false,
-          state: Default::default(),
-        });
-      }
-			Some(1)
-		} else {
-      // TODO get linear state mut mapped over commitedix > commitedix (lazy clear)
-      // then check linear ix opt.
-			unimplemented!()
+		if nb_branch == 0 {
+			return None;
 		}
+
+		// for 0 is the first branch creation case
+		if branch_ix != 0 {
+			// branch should not be dropped.
+			if let Some(branch) = self.access_branch_mut(branch_ix) {
+				branch.has_children = true;
+			} else {
+				return None;
+			}
+		}
+
+		let result_ix = self.last_branch_ix + 1;
+		for i in result_ix .. result_ix + nb_branch {
+			self.branches.insert(i, StatesBranch {
+				branch_ix: i,
+				origin_branch_ix: branch_ix,
+				has_children: false,
+				committed_ix: usize::max_value(),
+				prospective_ix: self.prospective_ix,
+				state: Default::default(),
+			});
+		}
+		self.last_branch_ix += nb_branch;
+
+		Some(result_ix)
 	}
 
 	pub fn get(&self, branch_ix: usize, linear_ix: usize) -> TransactionState {
@@ -169,18 +207,35 @@ impl States {
 	}
 
 	pub fn linear_state(&self, branch_ix: usize) -> Option<&LinearStates> {
-		unimplemented!();
+		self.branches.get(&branch_ix)
+			.filter(|b| !b.is_dropped_internal(self.prospective_ix, self.committed_ix))
+			.map(|b| &b.state)
 	}
 
+	// does remove branch if dropped.
+	fn access_branch_mut(&mut self, branch_ix: usize) -> Option<&mut StatesBranch> {
+		if let Some(branch) = self.branches.get(&branch_ix) {
+			if branch.is_dropped_internal(self.prospective_ix, self.committed_ix) {
+				let _ = self.branches.remove(&branch_ix);
+				return None;
+			} else {
+				return self.branches.get_mut(&branch_ix);
+			}
+		}
+		None
+	}
+
+
 	pub fn linear_state_mut (&mut self, branch_ix: usize) -> Option<&mut LinearStates> {
-    // TODO return state prospective, not commited, not branched
-		unimplemented!();
+		self.access_branch_mut(branch_ix)
+			.filter(|b| !b.has_children)
+			.map(|b| &mut b.state)
 	}
 
 }
 
 pub fn ref_get(s: &StatesRef, branch_ix: usize, linear_ix: usize) -> TransactionState {
-		unimplemented!();
+	unimplemented!();
 }
 
 #[cfg(test)]
@@ -190,20 +245,17 @@ mod test {
 	fn test_states() -> States {
 		let mut states = States::default();
 		assert_eq!(states.create_branch(0, 1), Some(1));
-    // root branching.
+		// root branching.
 		assert_eq!(states.create_branch(0, 1), Some(2));
-    // new txs
-    for _ in 0..3 {
-      states.linear_state_mut(1).map(|ls| ls.start_transaction());
-    }
 		assert_eq!(states.create_branch(1, 2), Some(3));
-    assert_eq!(states.linear_state_mut(1), None);
-    assert!(states.linear_state(1).is_some());
+		assert!(states.linear_state(1).is_some());
+		// new txs
+		states.linear_state_mut(1).map(|ls| ls.start_transaction());
 		states
 	}
 
 	#[test]
-  fn test_to_define() {
-    let states = test_states();
-  }
+	fn test_to_define() {
+		let states = test_states();
+	}
 }
