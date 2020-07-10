8db0a96e7fa8795cd6fb8ce5ab8563ea9b20e701
cheme <emericchevalier.pro@gmail.com>
2020-07-10 09:45:58 +0200

    hard assert

--- primitives/state-machine/src/trie_backend.rs
+++ primitives/state-machine/src/trie_backend.rs
@@ -88,11 +88,11 @@ impl<S: TrieBackendStorage<H>, H: Hasher> Backend<H> for TrieBackend<S, H> where
 		if self.alternative.assert_value() {
 			let reference = self.essence.storage(key);
 			let alter = self.alternative.storage(key);
-			if alter != reference {
+			/*if alter != reference {
 				warn!("###Different values in sm!!!!!!!!!!!! {:?}", key);
-			}
-			//assert!(alter == reference, "mistmatch in sm for key {:?}", key);
-			warn!("a match");
+			}*/
+			assert!(alter == reference, "mistmatch in sm for key {:?}", key);
+			//warn!("a match");
 			reference
 		} else {
 			self.essence.storage(key)
