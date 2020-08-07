f819032d930842bde3f80fed1eb34a5ff31c00bb
cheme <emericchevalier.pro@gmail.com>
2020-08-07 16:23:27 +0200

    fix

--- client/consensus/babe/src/lib.rs
+++ client/consensus/babe/src/lib.rs
@@ -1305,7 +1305,7 @@ impl<Block, Client, Inner> BlockImport<Block> for BabeBlockImport<Block, Client,
 			if let Some(shared_pruning_requirements) = self.shared_pruning_requirements.as_ref() {
 				let needed_height = epoch_changes.needed_parent_relation();
 				debug!(target: "babe", "Using prune limit {:?}", needed_height);
-				println!("Using prune limit {:?} for {:?}", needed_height, epoch_changes.epoch);
+				println!("Using prune limit {:?} for {:?}", needed_height, epoch_changes.epochs);
 				// TODO only set if changed!!
 				if let Some(height) = needed_height {
 					shared_pruning_requirements.set_finalized_headers_needed(PruningLimit::Some(height));
