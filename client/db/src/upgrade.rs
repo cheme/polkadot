aff4339f6230387cea0fbddd175827e836f7f8b5
cheme <emericchevalier.pro@gmail.com>
2020-09-03 10:00:24 +0200

    trying smaller ix

--- client/db/src/upgrade.rs
+++ client/db/src/upgrade.rs
@@ -284,7 +284,7 @@ fn compare_latest_roots<Block: BlockT>(db_path: &Path, db_type: DatabaseType, ha
 	let now = Instant::now();
 
 	let mut indexes = std::collections::BTreeMap::new();
-	let indexes_conf = trie_db::partial_db::DepthIndexes::new(&[80]);
+	let indexes_conf = trie_db::partial_db::DepthIndexes::new(&[40]);
 	let mut root_callback = trie_db::TrieRootIndexes::<HashFor<Block>, _, _>::new(&mut indexes, &indexes_conf);
 	let _state = management.get_db_state(&hash_for_root).expect("just added");
 	let iter_kv = historied_db.iter();
@@ -501,6 +501,7 @@ fn delete_historied<Block: BlockT>(db_path: &Path, db_type: DatabaseType) -> sp_
 			println!("in mem index calculated rocksdb write : {}", now.elapsed().as_millis());
 		}
 	};
+	/*
 	// last checked max length was 120 so 240 index
 	let all = & 
 [1u32, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239];
@@ -525,6 +526,7 @@ fn delete_historied<Block: BlockT>(db_path: &Path, db_type: DatabaseType) -> sp_
 	try_index(every_four, false);
 	try_index(every_five, false);
 	try_index(every_ten, false);
+	*/
 /*	try_index(&[8], false);
 	try_index(&[16], false);
 	try_index(&[64], false);
@@ -533,7 +535,7 @@ fn delete_historied<Block: BlockT>(db_path: &Path, db_type: DatabaseType) -> sp_
 	try_index(&[230], false);
 	try_index(&[80], true);*/
 
-	try_index(&[80], true);
+	try_index(&[40], true);
 	Ok(())
 }
 
