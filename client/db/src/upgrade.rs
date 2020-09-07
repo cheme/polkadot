68637984328f0774b03af2be2eb68c93cb76613d
cheme <emericchevalier.pro@gmail.com>
2020-09-07 12:08:25 +0200

    combine

--- client/db/src/upgrade.rs
+++ client/db/src/upgrade.rs
@@ -545,7 +545,8 @@ fn delete_historied<Block: BlockT>(db_path: &Path, db_type: DatabaseType) -> sp_
 	try_index(&[230], false);
 	try_index(&[80], true);*/
 
-	try_index(&[80], true);
+//	try_index(&[32, 64, 96], true);
+	try_index(&[65, 80], true);
 	Ok(())
 }
 
